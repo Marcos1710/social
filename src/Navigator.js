4cb993b3aebd4215de24ab8b4e3e8433a512dce8
Marcos Santos <marcossamuel1710@outlook.com>
2019-10-18 14:13:17 -0300

    corrigido toda a parte de navegacao, ao baixar o codigo do git tem que gerar chava para o projeto rodar

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react'
-import { 
-  createAppContainer,
-  createSwitchNavigator
- } from 'react-navigation'
+import React from 'react'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import { createStackNavigator } from 'react-navigation-stack'
-import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
 import AddPhoto from './screens/AddPhoto'
 import Profile from './screens/Profile'
 import Login from './screens/Login'
 import Register from './screens/Register'
+
 
 const AuthOrRouter = createStackNavigator({
   Login: {
@@ -36,41 +35,44 @@ const LoginOrProfileRouter = createSwitchNavigator({
     initialRouteName: 'Profile'
 })
 
-const Navigator = createStackNavigator({
-  Feed: {
-    name: 'Feed',
-    screen: LoginOrProfileRouter,
-    navigationOptions: {
-      header: null,
-      title: 'Feed',
-      tabBarIcon: ({ tintColor }) =>
-        <Icon name='home' size={30} color={tintColor} />
-    }
+const myTabNavigator = createBottomTabNavigator(
+  {
+    Home: Feed,
+    AddPhoto: AddPhoto,
+    Profile: LoginOrProfileRouter
   },
-  Add: {
-    name: 'AddPhoto',
-    screen: AddPhoto,
-    navigationOptions: {
-      header: null,
-      title: 'Add Picture',
-      tabBarIcon: ({ tintColor }) =>
-        <Icon name='camera' size={30} color={tintColor} />
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ /* focused, horizontal, */ tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = FontAwesome5;
+        let iconName;
+
+        if (routeName  === 'Home') {
+          iconName = 'home'
+        } else if (routeName === 'AddPhoto') {
+          iconName = 'camera'
+        } else if (routeName === 'Profile') {
+          iconName = 'user-alt'
+        }
+
+        return <IconComponent name={iconName}
+          size={23}
+          color={tintColor}
+          />
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: '#34495e',
+      inactiveTintColor: '#bdc3c7',
+      style: {
+        backgroundColor: 'ecf0f1'
+      },
+      showLabel: false // não aparece o nome em baixo do icone 
     }
-  },
-  Profile: {
-    name: 'Profile',
-    screen: Profile,
-    navigationOptions: {
-      header: null,
-      title: 'Profile',
-      tabBarIcon: ({ tintColor: color }) =>
-        <Icon name='user' size={30} color={color} />
-    }
-  }   
-})
-  
+  }
+)
 
+export default createAppContainer(myTabNavigator)
 
-export default createAppContainer(Navigator)
-
-// https://www.youtube.com/watch?v=XykhG0dgYIc
+// https://www.youtube.com/watch?v=XykhG0dgYIc e sempre ver as dependências que o react-navigations pede
