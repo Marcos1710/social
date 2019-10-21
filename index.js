4965c2243b5bf9fa545e72a162def318db270da7
Marcos Santos <marcossamuel1710@outlook.com>
2019-10-21 14:03:49 -0300

    inserído o redux ao projeto

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
-// import Feed from './src/screens/Feed'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { AppRegistry } from 'react-native';
 import Navigator from './src/Navigator'
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import storeConfig from './src/store/storeConfig'
 
-AppRegistry.registerComponent(appName, () => Navigator);
+const store = storeConfig()
+const Redux = () => {
+	return (
+		<Provider store={store} >
+			<Navigator />
+		</Provider>
+	)
+}
+
+AppRegistry.registerComponent(appName, () => Redux);
---
+++ src/store/reducers/user.js
@@ -0,0 +1,32 @@
+import {
+  USER_LOGGED_IN,
+  USER_LOGGED_OUT
+} from '../actions/actionTypes'
+
+const initiaState = {
+  name: null,
+  email: null
+}
+
+const reducer = (state = initiaState, action) => {
+  switch(action.type) {
+    case USER_LOGGED_IN: 
+      return {
+        ...state,
+        name: action.payload.name,
+        email: action.payload.email
+      } 
+    
+    case USER_LOGGED_OUT: 
+      return {
+        ...state,
+        name: null,
+        email: null
+      } 
+    
+    default: 
+      return state
+  }
+}
+
+export default reducer
