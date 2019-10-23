1e78e83965022e671caf24857833cb966f4a8c63
Marcos Santos <marcossamuel1710@outlook.com>
2019-10-23 17:31:59 -0300

    add comentario funcionando com redux

--- src/components/Post.js
+++ src/components/Post.js
@@ -12,7 +12,7 @@ class Post extends Component {
         <Author email={this.props.email} 
           nickname={this.props.nickname} />
         <Comments comments={this.props.comments} />
-        <AddComment />
+        <AddComment postId={this.props.id} />
       </View>
     )
   }
