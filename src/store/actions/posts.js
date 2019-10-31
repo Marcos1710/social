abb011572db51490fb83827397d955b5fbb7a2a0
Marcos Santos <marcossamuel1710@outlook.com>
2019-10-31 08:09:30 -0300

    alteracao

--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -2,15 +2,16 @@ import { ADD_POST, ADD_COMMENT } from './actionTypes'
 import axios from 'axios'
 
 export const addPost = post => {
-  return dispatch => {
+  /* return dispatch => {
     axios.post('/posts.json', { ...post })
       .catch(err => console.log(err))
       .then(res => console.log(res))
-  }
-  /* return {
+  } */
+  
+  return {
     type: ADD_POST,
     payload: post
-  } */
+  }
 }
 
 export const addComment = payload => {
